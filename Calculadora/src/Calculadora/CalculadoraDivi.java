d161bfc3073e6d8d958ed6ccd9cca70a918b65a6
Luís Felipe Pereira Da Silva <116446769+Felepenhos@users.noreply.github.com>
2023-11-06 20:29:39 -0300

    Add files via upload

---
+++ Calculadora/src/Calculadora/CalculadoraDivi.java
@@ -0,0 +1,21 @@
+package Calculadora;
+/**
+* @author luis felipe
+* 
+*
+*
+* 
+*  Uma classe simples de divisão  que realiza todas as operações desejadas 
+*/
+public class CalculadoraDivi {
+	/**
+	 * 
+	 * @param a O primeiro operando
+	 * @param b o segundo operando
+	 * @return O resultado da operação
+	 */
+	public int divi(int a, int b){
+        return a / b ; 
+
+}
+}
