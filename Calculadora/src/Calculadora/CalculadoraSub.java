64eb770c37cfa08fe1dc80564c85013bba6829c9
Luís Felipe Pereira Da Silva <116446769+Felepenhos@users.noreply.github.com>
2023-11-06 20:33:39 -0300

    Add files via upload

--- Calculadora/src/Calculadora/CalculadoraSub.java
+++ Calculadora/src/Calculadora/CalculadoraSub.java
@@ -1,7 +1,22 @@
 package Calculadora;
+/**
+* @author luis felipe
+* @version 1.05
+* @since release 1.02 da aplicacão
+*
+*
+* 
+*  Uma classe simples de subtração  que realiza todas as operações desejadas 
+*/
+
 
 public class CalculadoraSub {
-
+	/**
+	 * 
+	 * @param a O primeiro operando
+	 * @param b o segundo operando
+	 * @return O resultado da operação
+	 */
 	     public int sub(int a, int b){
 	        return a - b ; 
 
